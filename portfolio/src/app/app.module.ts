a36d9e37fe21dd21ec2dce82e01cc520cbd3b4df
Zarkan69 <zarkan69@gmail.com>
2024-03-06 03:30:45 +0100

    random changes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ParcoursComponent } from './parcours/parcours.component';
+import { CompetencesComponent } from './competences/competences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    ParcoursComponent,
+    CompetencesComponent,
   ],
   imports: [
     BrowserModule,
